3034a7372fa69b29b4b0ab355e660396ef03d186
Patrick Ngo <patrick.ngo@hotmail.com>
2017-12-17 00:50:36 +0800

    remove main.storyboard in AppDelegate, setup rootViewController programatically

--- resume-ios/AppDelegate.swift
+++ resume-ios/AppDelegate.swift
@@ -15,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.makeKeyAndVisible()
+        
+        self.window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        
         // Override point for customization after application launch.
         return true
     }
