16d185f13b2779de1ca2b64dc7fc7d4b96517b6a
Patrick Ngo <patrick.ngo@hotmail.com>
2017-12-17 00:20:02 +0800

    Initial Commit

---
+++ resume-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  resume-ios
+//
+//  Created by Patrick Ngo on 2017-12-17.
+//  Copyright © 2017 Patrick Ngo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ resume-iosTests/resume_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  resume_iosTests.swift
+//  resume-iosTests
+//
+//  Created by Patrick Ngo on 2017-12-17.
+//  Copyright © 2017 Patrick Ngo. All rights reserved.
+//
+
+import XCTest
+@testable import resume_ios
+
+class resume_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
