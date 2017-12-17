9707d49ed40ff1e1396592b8808770af957b94ac
Patrick Ngo <patrick.ngo@hotmail.com>
2017-12-17 11:13:23 +0800

    added extensions for CAGradientLayer and UINavigationBar for easy creation of a gradient background

---
+++ resume-ios/Common/Extensions/CAGradientLayer+Image.swift
@@ -0,0 +1,37 @@
+//
+//  CAGradientLayer+Image.swift
+//  resume-ios
+//
+//  Created by Patrick Ngo on 2017-12-17.
+//  Copyright © 2017 Patrick Ngo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension CAGradientLayer {
+    
+    convenience init(frame: CGRect, colors: [UIColor]) {
+        self.init()
+        self.frame = frame
+        self.colors = []
+        for color in colors {
+            self.colors?.append(color.cgColor)
+        }
+        startPoint = CGPoint(x: 0, y: 0)
+        endPoint = CGPoint(x: 0, y: 1)
+    }
+    
+    func createGradientImage() -> UIImage? {
+        
+        var image: UIImage? = nil
+        UIGraphicsBeginImageContext(bounds.size)
+        if let context = UIGraphicsGetCurrentContext() {
+            render(in: context)
+            image = UIGraphicsGetImageFromCurrentImageContext()
+        }
+        UIGraphicsEndImageContext()
+        return image
+    }
+    
+}
---
+++ resume-ios/Common/Extensions/UINavigationBar+Gradient.swift
@@ -0,0 +1,25 @@
+//
+//  UINavigationBar+Gradient.swift
+//  resume-ios
+//
+//  Created by Patrick Ngo on 2017-12-17.
+//  Copyright © 2017 Patrick Ngo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UINavigationBar {
+    
+    func setGradientBackground(colors: [UIColor]) {
+        
+        var updatedFrame = bounds
+        updatedFrame.size.height += 20
+        let gradientLayer = CAGradientLayer(frame: updatedFrame, colors: colors)
+        
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+
+        setBackgroundImage(gradientLayer.createGradientImage(), for: UIBarPosition.any, barMetrics: UIBarMetrics.default)
+    }
+}
